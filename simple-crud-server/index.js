3e794a60c3cd1530cd16e18ebd553ebcc166fbf6
Md Bocktear Abid Borat <boratabid@gmail.com>
2025-11-18 23:14:06 +0600

    commit-1

--- simple-crud-server/index.js
+++ simple-crud-server/index.js
@@ -5,34 +5,32 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors({
-  origin: "http://localhost:5173",
-}));
+app.use(cors());
 app.use(express.json());
 
-// MongoDB URI
+// MONGO URI
 const uri = "mongodb+srv://borat156006_db_user:fMzQNQcyg1sQ5VTR@cluster0.i8wpz9p.mongodb.net/?retryWrites=true&w=majority&tls=true&appName=Cluster0";
 
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
+    strict: false,
+    deprecationErrors: false,
   },
 });
 
 async function run() {
   try {
     await client.connect();
+    console.log("MongoDB Connected ✔");
+
     const userCollection = client.db("simpleCardDB").collection("users");
 
-    // GET API
     app.get('/users', async (req, res) => {
       const users = await userCollection.find().toArray();
       res.send(users);
     });
 
-    // POST API
     app.post('/users', async (req, res) => {
       const user = req.body;
       const result = await userCollection.insertOne(user);
@@ -40,12 +38,11 @@ async function run() {
     });
 
     app.get('/', (req, res) => {
-      res.send("SERVER IS RUNNING ✔");
+      res.send("SERVER IS RUNNING");
     });
 
-    console.log("MongoDB Connected ✔");
   } catch (error) {
-    console.log(error);
+    console.log("Mongo Error ❌", error);
   }
 }
 
